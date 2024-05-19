4c7230f19e5c0222876987a30416dacb5a4e24c5
Azizidimas <116472583+Azizidimas@users.noreply.github.com>
2024-05-20 00:39:50 +0700

    Add files via upload

---
+++ Pemograman Website/week8/basic.php
@@ -0,0 +1,20 @@
+<?php
+    echo "<p>Kalimat ini berasal dari Standard PHP tag </p>";
+
+    echo "<p>Hello World</p>";
+    ECHO "<p>Hello World</p>";
+    EcHo "<p>Hello World</p>";
+
+    $andi = "Andi";
+    echo $Andi;
+    
+    echo "<p>Hello world</p>";
+    $a = 3; $b = 4;
+    $nama = "Informatika";
+    $c = $a / 25.0;
+    if ($a != $b) {
+    echo "<p>Pemrograman Web</p>";
+    }
+    // Parse Error (Semicolon)
+    // $a = 5 + 7
+    // echo $a;
